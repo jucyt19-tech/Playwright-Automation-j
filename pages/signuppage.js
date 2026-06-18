63b3ffa944fb431e6b30fb2c52e616959fa9f6c9
jucy19-tech <jucyt19@gmail.com>
2026-06-18 16:48:48 +0530

    new test run

--- pages/signuppage.js
+++ pages/signuppage.js
@@ -6,10 +6,6 @@ export class Signuppage{
         this.signuplink=page.locator('#signin2')
         this.signupbutton=page.getByRole('button',{name:'Sign up'})
         this.closebutton=page.getByRole('button',{name:'Close'}).nth(0)
-        this.loginlink=page.locator('#login2')
-        this.loginusername=page.locator('#loginusername')
-        this.loginpassword=page.locator('#loginpassword')
-        this.loginbutton=page.getByRole('button',{name:'Log in'})
     }
 
     async gotourl(){
@@ -30,25 +26,5 @@ export class Signuppage{
 
     async clickclosebutton(){
         await this.closebutton.click()
-    }
-    async clickloginlink(){
-        await this.loginlink.click()
-    }
-
-    async loginusernamepassword(username,password){
-        await this.loginusername.fill(username)
-        await this.loginpassword.fill(password)
-    }
-
-    async clickloginbutton(){
-        await this.loginbutton.click()
-    }
-
-    async logintoapp(username,password){
-        await this.page.goto("https://demoblaze.com/")
-        await this.loginlink.click()
-        await this.loginusername.fill(username)
-        await this.loginpassword.fill(password)
-        await this.loginbutton.click()
     }
 }
